04a0499564589659d75cfd27a984ac0430493dd2
Renan Souza <renan.souza@powerpush.com.br>
2025-05-02 17:21:47 -0300

    first commit

---
+++ Source/Plugin_Multiplayer/Plugin_Multiplayer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Plugin_Multiplayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Plugin_Multiplayer, "Plugin_Multiplayer" );
