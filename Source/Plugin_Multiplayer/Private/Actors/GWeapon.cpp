8be6c5374a2049b1b7a9c170d42e1d9d29bb20c3
Renan Souza <renan.souza@powerpush.com.br>
2025-05-06 22:08:51 -0300

    adding the base game weapon actor

---
+++ Source/Plugin_Multiplayer/Private/Actors/GWeapon.cpp
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/GWeapon.h"
+
+#include "Components/SphereComponent.h"
+
+AGWeapon::AGWeapon() {
+	this->PrimaryActorTick.bCanEverTick = false;
+
+	// Define the Actor Authority and server replication
+	this->bReplicates = true;
+
+	this->WeaponMesh = this->CreateDefaultSubobject<UStaticMeshComponent>("Weapon Mesh");
+	this->SetRootComponent(this->WeaponMesh);
+	this->WeaponMesh->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Block);
+	this->WeaponMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+	
+	// This is the initial state of collision so players dont trip on the gun. -Renan 
+	this->WeaponMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	this->PlayerDetectionSphere = this->CreateDefaultSubobject<USphereComponent>("Player Detection Sphere");
+	this->PlayerDetectionSphere->SetupAttachment(this->GetRootComponent());
+
+	// Disables the player detection sphere collision to be sure this only runs on the server. -Renan
+	this->PlayerDetectionSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	this->PlayerDetectionSphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AGWeapon::BeginPlay() {
+	Super::BeginPlay();
+
+	// If this actor is running on the server, them I enable the collision of the player detection sphere. -Renan
+	if (this->HasAuthority()) {
+		this->PlayerDetectionSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+		this->PlayerDetectionSphere->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	}
+}
+
+void AGWeapon::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Plugin_Multiplayer/Public/Actors/GWeapon.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GWeapon.generated.h"
+
+UCLASS()
+class PLUGIN_MULTIPLAYER_API AGWeapon : public AActor {
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Actor Components")
+	TObjectPtr<class USphereComponent> PlayerDetectionSphere;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Actor Components")
+	TObjectPtr<class UStaticMeshComponent> WeaponMesh;
+	
+public:
+	AGWeapon();
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+};
